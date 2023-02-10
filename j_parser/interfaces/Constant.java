81c1ec910a58cbd0659b304c78f58d4394d65f73
Dragon <49222054+towelieTowel@users.noreply.github.com>
2023-02-10 14:43:36 -0500

    restructred module structure

---
+++ j_parser/interfaces/Constant.java
@@ -0,0 +1,6 @@
+package JParser.j_parser.interfaces;
+
+/*Interface for all constant pool types*/
+public interface Constant{
+    String getType(); //@return name of the constant as defined in the source
+}
--- j_parser/types/Constant.java
+++
@@ -1,7 +0,0 @@
-package j-parser.types;
-
-/*Interface for all constant types*/
-public interface Constant{
-    String getName(); //@return must be the Name of the constant
-    String getType(); //@return must be the type of constant i.e. CONST_UTF_8_info
-}
