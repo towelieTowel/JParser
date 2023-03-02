5e384ae3fd34872d54d30440091a6968955362b7
Dragon <49222054+towelieTowel@users.noreply.github.com>
2023-03-02 16:47:15 -0500

    implemented ConstantDynamic

---
+++ j_parser/types/constants/ConstantDynamic.java
@@ -0,0 +1,36 @@
+package j_parser.types.constants;
+
+import j_parser.enums.ConstantTag;
+import j_parser.interfaces.Constant;
+
+final public class ConstantDynamic implements Constant{
+    private final int TAG = ConstantTag.DYNAMIC.TAG;
+    private final short BOOTSTRAP_METHOD_ATTR_INDEX;
+    private final short NT_INDEX;
+    private final String COMMON_NAME = ConstantTag.DYNAMIC.name();
+
+    public ConstantDynamic(short bootstrapMethodAttrIndex, short ntIndex){
+        this.BOOTSTRAP_METHOD_ATTR_INDEX = bootstrapMethodAttrIndex;
+        this.NT_INDEX = ntIndex;
+    }
+    
+    public short getBootstrapMethodAttrIndex(){
+        return this.BOOTSTRAP_METHOD_ATTR_INDEX;
+    } 
+
+    public short getNTIndex(){
+        return this.NT_INDEX;
+    }
+
+    @Override
+    public String getCommonName(){
+        /*Required by Constant interface*/
+        return this.COMMON_NAME;
+    }
+    
+    @Override
+    public int getTag(){
+        /*Required by Constant interface*/
+        return this.TAG;
+    }
+}
