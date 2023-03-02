bf3110d4bd0ee4cd5a96dcaa539a07a716259b50
Dragon <49222054+towelieTowel@users.noreply.github.com>
2023-03-02 15:42:42 -0500

    implemented ConstantNameAndType

---
+++ j_parser/types/constants/ConstantNameAndType.java
@@ -0,0 +1,36 @@
+package j_parser.types.constants;
+
+import j_parser.enums.ConstantTag;
+import j_parser.interfaces.Constant;
+
+final public class ConstantNameAndType implements Constant{
+    private final int TAG = ConstantTag.NAMEANDTYPE.TAG;
+    private final short NAME_INDEX;
+    private final short DESCRIPTOR_INDEX;
+    private final String COMMON_NAME = ConstantTag.NAMEANDTYPE.name();
+
+    public ConstantNameAndType(short nameIndex, short descriptorIndex){
+        this.NAME_INDEX = nameIndex;
+        this.DESCRIPTOR_INDEX = descriptorIndex;
+    }
+
+    public short getNameIndex(){
+        return this.NAME_INDEX;
+    }
+
+    public short getDescriptorIndex(){
+        return this.DESCRIPTOR_INDEX;
+    }
+
+    @Override
+    public String getCommonName(){
+        /*Required by Constant interface*/
+        return this.COMMON_NAME;
+    }
+    
+    @Override
+    public int getTag(){
+        /*Required by Constant interface*/
+        return this.TAG;
+    }
+}
