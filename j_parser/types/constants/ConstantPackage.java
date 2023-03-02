3fa3161f0f6d05bacd355689921a24de6842f500
Dragon <49222054+towelieTowel@users.noreply.github.com>
2023-03-02 16:53:38 -0500

    implemented ConstantPackage

---
+++ j_parser/types/constants/ConstantPackage.java
@@ -0,0 +1,30 @@
+package j_parser.types.constants;
+
+import j_parser.enums.ConstantTag;
+import j_parser.interfaces.Constant;
+
+final public class ConstantPackage implements Constant{
+    private final int TAG = ConstantTag.PACKAGE.TAG;
+    private final short NAME_INDEX; 
+    private final String COMMON_NAME = ConstantTag.PACKAGE.name();
+
+    public ConstantPackage(short nameIndex){
+        this.NAME_INDEX = nameIndex;
+    }
+    
+    public short getNameIndex(){
+        return this.NAME_INDEX;
+    } 
+
+    @Override
+    public String getCommonName(){
+        /*Required by Constant interface*/
+        return this.COMMON_NAME;
+    }
+    
+    @Override
+    public int getTag(){
+        /*Required by Constant interface*/
+        return this.TAG;
+    }
+}
