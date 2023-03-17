46ed20b4ad6f08f0a71decb3936e442a6477bb13
towelieTowel <49222054+towelieTowel@users.noreply.github.com>
2023-03-17 02:00:22 -0400

    Fix unwanted changes due to merge conflict
    Unwanted changes to this branch were added from a merge conflict with
    the main branch. These changes were needed to resolve the conflict.

--- src/main/java/j_parser/ClassFile.java
+++ src/main/java/j_parser/ClassFile.java
@@ -16,29 +16,11 @@ public class ClassFile{
     private final int minorVersion;
     private final int majorVersion;
     private final ConstantPool cPool;
-<<<<<<< HEAD
-    private final short constantCount;
-    private final short accessFlags;
-    private final short thisClass;
-    private final short superClass;
-    
-=======
     private final int constantCount;
-/*
     private final int accessFlags;
     private final int thisClass;
-
     private final int superClass;
-    private final int interfacesCount;
-    private int[] interfaces;
-    private int fieldsCount;
-    private HashMap<int, Field> fields;
-    private int methodsCount;
-    private HashMap<int, Method> methods;
-    private int attributesCount;
-    private HashMap<int, Attribute> attributes;
-*/
->>>>>>> main
+    
     public ClassFile(File classFile) throws IOException{
         DataInputStream fileStream = new DataInputStream(new FileInputStream(classFile));
 
@@ -47,22 +29,21 @@ public class ClassFile{
         this.majorVersion = fileStream.readUnsignedShort();
         this.cPool = new ConstantPool(fileStream); 
         this.constantCount = this.cPool.getConstantCount();
-        this.accessFlags = (short)fileStream.readUnsignedShort();
-        this.thisClass = (short)fileStream.readUnsignedShort();
-        this.superClass = (short)fileStream.readUnsignedShort();
+        this.accessFlags = fileStream.readUnsignedShort();
+        this.thisClass = fileStream.readUnsignedShort();
+        this.superClass = fileStream.readUnsignedShort();
    }
 
     public int getMagicNumber(){return this.magicNumber;}
     
     public int getMinorVersion(){return this.minorVersion;}
     
-<<<<<<< HEAD
-    public short getMajorVersion(){return this.majorVersion;}
+    public int getMajorVersion(){return this.majorVersion;}
    
     public ConstantPool getConstantPool(){return this.cPool;}
  
-    public short getConstantCount(){return this.constantCount;}
-    public short getRawAccessFlag(){return this.accessFlags;}
+    public int getConstantCount(){return this.constantCount;}
+    public int getRawAccessFlag(){return this.accessFlags;}
     
     public ArrayList<String> getAccessFlags(){
         ArrayList<String> flagsList = new ArrayList<>();
@@ -75,14 +56,7 @@ public class ClassFile{
         return flagsList;
     }
 
-    public short getThisClass(){return this.thisClass;}
-
-//    public short getSuperClass(){return this.superClass;}
-=======
-    public int getMajorVersion(){return this.majorVersion;}
-    
-    public int getConstantCount(){return this.constantCount;}
->>>>>>> main
+ //   public int getThisClass(){return this.thisClass;}
     
     public ArrayList<String> getStrings(){
         return this.cPool.getStrings();
--- src/main/java/j_parser/utils/enums/AccessFlags.java
+++ src/main/java/j_parser/utils/enums/AccessFlags.java
@@ -1,19 +1,19 @@
 package j_parser.utils.enums;
 
 public enum AccessFlags{
-    ACC_PUBLIC((short)0x0001),
-    ACC_FINAL((short)0x0010),
-    ACC_SUPER((short)0x0020),
-    ACC_INTERFACE((short)0x0200),
-    ACC_ABSTRACT((short)0x0400),
-    ACC_SYNTHETIC((short)0x1000),
-    ACC_ANNOTATION((short)0x2000),
-    ACC_ENUM((short)0x4000),
-    ACC_MODULE((short)0x8000);
+    ACC_PUBLIC(0x0001),
+    ACC_FINAL(0x0010),
+    ACC_SUPER(0x0020),
+    ACC_INTERFACE(0x0200),
+    ACC_ABSTRACT(0x0400),
+    ACC_SYNTHETIC(0x1000),
+    ACC_ANNOTATION(0x2000),
+    ACC_ENUM(0x4000),
+    ACC_MODULE(0x8000);
 
-    public final short FLAG;
+    public final int FLAG;
 
-    AccessFlags(short flag){
+    AccessFlags(int flag){
         this.FLAG = flag;
     }
 }
