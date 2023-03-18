8d6eed067444b2d4a392b05cf0f638cd134ea4a6
towelieTowel <49222054+towelieTowel@users.noreply.github.com>
2023-03-18 19:45:07 -0400

    Refactor add buildDocs gradle task
        Added a task to lib/build.gradle to generate documentation and override
        the default /build/javadoc directory as the target for the generated
        documentation. Also, deleted top-level docs directory.
    
        It is important that the users have access to the documentation. Since all
        files in the /build directory are ignored by git, the documentation needed
        to be placed outside /build.

---
+++ jparser/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"j_parser"},{"l":"j_parser.interfaces"},{"l":"j_parser.types.constants"},{"l":"j_parser.utils"},{"l":"j_parser.utils.enums"}];updateSearchResults();
