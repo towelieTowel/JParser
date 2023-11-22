0640c33ba9152f639309dc954781efa6ba724700
towelieTowel <49222054+towelieTowel@users.noreply.github.com>
2023-11-22 18:15:00 -0500

    Move constant pool parsing logic into the ConstantPool
    Moved the logic inside CPoolParser into ConstantPool. Removed CPoolParser.java.

--- jparser/lib/src/main/java/j_parser/ConstantPool.java
+++ jparser/lib/src/main/java/j_parser/ConstantPool.java
@@ -7,7 +7,6 @@ import java.lang.Integer;
 import j_parser.utils.ConstantTag;
 import j_parser.interfaces.Constant;
 import j_parser.types.constants.*;
-import j_parser.utils.CPoolParser;
 
 /** ConstantPool models the constant pool table as specified by the Java Virtual
  * Machine documentation. This class provides the user with an interface for
@@ -46,7 +45,6 @@ public class ConstantPool{
         Java Virtual Machine Specification: 4.4 The Constant Pool </a>
     */
     public ConstantPool(DataInputStream in, int constantPoolCount) throws IOException{
-        CPoolParser parser = new CPoolParser( in );
 
         //Every constant pool table entry is mapped to reflect a normal index
         //range for an array. I.E. Constant[ 0 ] ... [ n ].
@@ -54,7 +52,8 @@ public class ConstantPool{
         this.cPool = new Constant[constantPoolCount - 1];
         
         for ( int i = 0; i <= this.cPool.length - 1; ++i ) {
-            this.cPool[ i ] = parser.parse();
+            
+			this.cPool[ i ] = ConstantPool.parse( in );
             ++this.constantCount;
 
             int tag = this.cPool[ i ].getTag();
@@ -62,9 +61,15 @@ public class ConstantPool{
                 ( tag == ConstantTag.DOUBLE.TAG ) ) {
              
                 /*
-                    Long and Double constants occupy 2 entries in the constant pool
-                    The second entry is a valid entry but is not used
+                    Long and Double constants occupy 2 entries in the constant
+					pool. If ConstantLong is at n, then the next constant
+					structure will be at n + 2, not n + 1. 
+
+					See Section 4.4.5 in the Java Virtual Machine Specification,
+					Java SE 21. 
+					https://docs.oracle.com/javase/specs/jvms/se21/html/jvms-4.html#jvms-4.4.5
                 */
+
                 ++i;
             }
         }
@@ -123,5 +128,192 @@ public class ConstantPool{
     */
     public Constant[] getRawCPool(){
         return this.cPool;
+    }
+
+
+//Private methods
+//------------------------------------------------------------------------
+
+	 private static Constant parse( DataInputStream in ) throws IOException {
+		Constant nullConstant = null;
+        int currentTag = in.readUnsignedByte();
+        
+        if (currentTag == ConstantTag.UTF.TAG){
+            return buildConstantUTF(in);
+        } 
+        else if (currentTag == ConstantTag.INTEGER.TAG){
+            return buildConstantInteger(in); 
+        }
+        else if (currentTag == ConstantTag.FLOAT.TAG){
+            return buildConstantFloat(in);
+        }
+        else if (currentTag == ConstantTag.LONG.TAG){
+            return buildConstantLong(in);
+        }
+        else if (currentTag == ConstantTag.DOUBLE.TAG){
+            return buildConstantDouble(in);
+        }
+        else if (currentTag == ConstantTag.CLASS.TAG){
+            return buildConstantClass(in);
+
+        }
+        else if (currentTag == ConstantTag.STRING.TAG){
+            return buildConstantString(in);
+
+        }
+        else if (currentTag == ConstantTag.FIELDREF.TAG){
+            return buildConstantFieldRef(in);
+
+        }
+        else if (currentTag == ConstantTag.METHODREF.TAG){
+            return buildConstantMethodRef(in);
+
+        }
+        else if (currentTag == ConstantTag.INTERFACE_METHODREF.TAG){
+            return buildConstantInterfaceMethodRef(in);
+
+        }
+        else if (currentTag == ConstantTag.NAMEANDTYPE.TAG){
+            return buildConstantNameAndType(in);
+
+        }
+        else if (currentTag == ConstantTag.METHODHANDLE.TAG){
+            return buildConstantMethodHandle(in);
+
+        }
+        else if (currentTag == ConstantTag.METHODTYPE.TAG){
+            return buildConstantMethodType(in);
+
+        }
+        else if (currentTag == ConstantTag.DYNAMIC.TAG){
+            return buildConstantDynamic(in);
+
+        }
+        else if (currentTag == ConstantTag.INVOKEDYNAMIC.TAG){
+            return buildConstantInvokeDynamic(in);
+
+        }
+        else if (currentTag == ConstantTag.MODULE.TAG){
+            return buildConstantModule(in);
+
+        }
+        else if (currentTag == ConstantTag.PACKAGE.TAG){
+            return buildConstantPackage(in);
+
+        }
+        else {
+            return nullConstant;
+        } 
+    }
+
+    private static ConstantUTF buildConstantUTF(DataInputStream in) throws IOException {
+        int len = in.readUnsignedShort();
+        byte[] data = new byte[len];
+        in.read(data, 0, len);
+
+        return new ConstantUTF(len, data);
+    }
+
+    private static ConstantInteger buildConstantInteger(DataInputStream in) throws IOException {
+        int value = in.readInt();
+        
+        return new ConstantInteger(value);
+    }
+
+    private static ConstantFloat buildConstantFloat(DataInputStream in) throws IOException {
+        float value = in.readFloat();
+
+        return new ConstantFloat(value);
+    }
+
+    private static ConstantLong buildConstantLong(DataInputStream in) throws IOException {
+        long value = in.readLong();
+
+        return new ConstantLong(value);
+    }
+
+    private static ConstantDouble buildConstantDouble(DataInputStream in) throws IOException {
+        double value = in.readDouble();
+
+        return new ConstantDouble(value);
+    }
+
+    private static ConstantClass buildConstantClass(DataInputStream in) throws IOException {
+        int nameIndex = in.readUnsignedShort();
+
+        return new ConstantClass(nameIndex);
+    }
+
+    private static ConstantString buildConstantString(DataInputStream in) throws IOException {
+        int stringIndex = in.readUnsignedShort();
+
+        return new ConstantString(stringIndex);
+    }
+
+    private static ConstantFieldRef buildConstantFieldRef(DataInputStream in) throws IOException {
+        int classIndex = in.readUnsignedShort();
+        int ntIndex = in.readUnsignedShort();
+
+        return new ConstantFieldRef(classIndex, ntIndex);
+    }
+
+    private static ConstantMethodRef buildConstantMethodRef(DataInputStream in) throws IOException {
+        int classIndex = in.readUnsignedShort();
+        int ntIndex = in.readUnsignedShort();
+
+        return new ConstantMethodRef(classIndex, ntIndex);
+    }
+
+    private static ConstantInterfaceMethodRef buildConstantInterfaceMethodRef(DataInputStream in) throws IOException {
+        int classIndex = in.readUnsignedShort();
+        int ntIndex = in.readUnsignedShort();
+
+        return new ConstantInterfaceMethodRef(classIndex, ntIndex);
+    }
+
+    private static ConstantNameAndType buildConstantNameAndType(DataInputStream in) throws IOException {
+        int nameIndex = in.readUnsignedShort();
+        int descriptorIndex = in.readUnsignedShort();
+        
+        return new ConstantNameAndType(nameIndex, descriptorIndex);
+    }
+
+    private static ConstantMethodHandle buildConstantMethodHandle(DataInputStream in) throws IOException {
+        int refKind = in.readUnsignedShort();
+        int index = in.readUnsignedShort();
+
+        return new ConstantMethodHandle(refKind, index);
+    }
+
+    private static ConstantMethodType buildConstantMethodType(DataInputStream in) throws IOException {
+        int descriptorIndex = in.readUnsignedShort();
+
+        return new ConstantMethodType(descriptorIndex);
+    }
+
+    private static ConstantDynamic buildConstantDynamic(DataInputStream in) throws IOException {
+        int bootstrapMethodAttrIndex = in.readUnsignedShort();
+        int ntIndex = in.readUnsignedShort();
+
+        return new ConstantDynamic(bootstrapMethodAttrIndex, ntIndex);
+    }
+
+    private static ConstantInvokeDynamic buildConstantInvokeDynamic(DataInputStream in) throws IOException {
+        int bootstrapMethodAttrIndex = in.readUnsignedShort();
+        int ntIndex = in.readUnsignedShort();
+
+        return new ConstantInvokeDynamic(bootstrapMethodAttrIndex, ntIndex);
+    }
+
+    private static ConstantModule buildConstantModule(DataInputStream in) throws IOException{
+        int nameIndex = in.readUnsignedShort();
+
+        return new ConstantModule(nameIndex);
+    }
+    
+    private static ConstantPackage buildConstantPackage(DataInputStream in) throws IOException{
+        int nameIndex = in.readUnsignedShort();
+
+        return new ConstantPackage(nameIndex);
     }
 }
